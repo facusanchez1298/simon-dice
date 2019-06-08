a7edacbfe7ae09aaf8c898c9df18c7b07084087f
Facundo Sanchez <fas_facu@hotmail.com>
2019-06-08 12:18:53 -0300

    ahora anda

--- SimonDice/Form1.Designer.cs
+++ SimonDice/Form1.Designer.cs
@@ -29,12 +29,111 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnAzul = new System.Windows.Forms.Button();
+            this.btnVerde = new System.Windows.Forms.Button();
+            this.btnRojo = new System.Windows.Forms.Button();
+            this.btnAmarrillo = new System.Windows.Forms.Button();
+            this.buttonJugar_click = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAzul
+            // 
+            this.btnAzul.BackColor = System.Drawing.Color.Blue;
+            this.btnAzul.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAzul.Location = new System.Drawing.Point(166, 40);
+            this.btnAzul.Name = "btnAzul";
+            this.btnAzul.Size = new System.Drawing.Size(110, 110);
+            this.btnAzul.TabIndex = 0;
+            this.btnAzul.Text = " ";
+            this.btnAzul.UseVisualStyleBackColor = false;
+            this.btnAzul.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnVerde
+            // 
+            this.btnVerde.BackColor = System.Drawing.Color.Green;
+            this.btnVerde.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerde.Location = new System.Drawing.Point(166, 260);
+            this.btnVerde.Name = "btnVerde";
+            this.btnVerde.Size = new System.Drawing.Size(110, 110);
+            this.btnVerde.TabIndex = 1;
+            this.btnVerde.Text = " ";
+            this.btnVerde.UseVisualStyleBackColor = false;
+            this.btnVerde.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnRojo
+            // 
+            this.btnRojo.BackColor = System.Drawing.Color.Red;
+            this.btnRojo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRojo.Location = new System.Drawing.Point(56, 150);
+            this.btnRojo.Name = "btnRojo";
+            this.btnRojo.Size = new System.Drawing.Size(110, 110);
+            this.btnRojo.TabIndex = 2;
+            this.btnRojo.Text = " ";
+            this.btnRojo.UseVisualStyleBackColor = false;
+            this.btnRojo.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnAmarrillo
+            // 
+            this.btnAmarrillo.BackColor = System.Drawing.Color.Yellow;
+            this.btnAmarrillo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAmarrillo.Location = new System.Drawing.Point(276, 150);
+            this.btnAmarrillo.Name = "btnAmarrillo";
+            this.btnAmarrillo.Size = new System.Drawing.Size(110, 110);
+            this.btnAmarrillo.TabIndex = 3;
+            this.btnAmarrillo.Text = " ";
+            this.btnAmarrillo.UseVisualStyleBackColor = false;
+            this.btnAmarrillo.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // buttonJugar
+            // 
+            this.buttonJugar_click.BackColor = System.Drawing.Color.White;
+            this.buttonJugar_click.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonJugar_click.Location = new System.Drawing.Point(166, 150);
+            this.buttonJugar_click.Name = "buttonJugar";
+            this.buttonJugar_click.Size = new System.Drawing.Size(110, 110);
+            this.buttonJugar_click.TabIndex = 0;
+            this.buttonJugar_click.Text = "jugar";
+            this.buttonJugar_click.UseVisualStyleBackColor = false;
+            this.buttonJugar_click.Click += new System.EventHandler(this.buttonJugar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(428, 411);
+            this.Controls.Add(this.btnAmarrillo);
+            this.Controls.Add(this.btnRojo);
+            this.Controls.Add(this.btnVerde);
+            this.Controls.Add(this.buttonJugar_click);
+            this.Controls.Add(this.btnAzul);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAzul;
+        private System.Windows.Forms.Button btnVerde;
+        private System.Windows.Forms.Button btnRojo;
+        private System.Windows.Forms.Button btnAmarrillo;
+        private System.Windows.Forms.Button buttonJugar_click;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- SimonDice/Form1.cs
+++ SimonDice/Form1.cs
@@ -12,9 +12,130 @@ namespace SimonDice
 {
     public partial class Form1 : Form
     {
+        int presionado;
+        List<int> juego = new List<int>();
+        int i = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+            agregarAleatorio();
         }
+
+        /// <summary>
+        /// vuelve todos los botones a sus colores originales
+        /// </summary>
+        public void apagarTodo()
+        {
+            btnAmarrillo.BackColor = Color.Yellow;
+            btnAzul.BackColor = Color.Blue;
+            btnRojo.BackColor = Color.Red;
+            btnVerde.BackColor = Color.Green;
+        }
+
+        /// <summary>
+        /// inicia el juego
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonJugar_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        public void agregarAleatorio()
+        {
+            Random random = new Random();
+
+            juego.Add(random.Next(1,5));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mostrar(juego.ElementAt(i));
+
+            if (i < juego.Count - 1) i++;
+            else
+            {
+                timer1.Stop();
+                i = 0;
+            }
+
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+             apagarTodo();
+            timer2.Stop();
+        }
+
+        public void mostrar(int boton)
+        {
+            if (boton == 1) btnAzul.BackColor = Color.DodgerBlue;
+            else if (boton == 2) btnAmarrillo.BackColor = Color.Khaki;
+            else if (boton == 3) btnVerde.BackColor = Color.LightGreen;
+            else if (boton == 4) btnRojo.BackColor = Color.IndianRed;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                Control control = (Control)sender;
+
+                if (control == btnAzul)
+                {
+                    presionado = 1;
+                }
+                else if (control == btnAmarrillo)
+                {
+                    presionado = 2;
+                }
+                else if (control == btnVerde)
+                {
+                    presionado = 3;
+                }
+                else if (control == btnRojo)
+                {
+                    presionado = 4;
+                }
+
+                if (continuar())
+                {
+                    if (i < juego.Count - 1) i++;
+                    else
+                    {
+                        agregarAleatorio();
+                        i = 0;
+                        buttonJugar_Click(sender, e);
+                    }
+                }
+                else
+                {
+                    mostrarMensaje("Perdiste");
+                    i = 0;
+                    timer1.Stop();
+                    timer2.Stop();
+                    juego.Clear();
+                }
+            }
+        }
+
+        public void mostrarMensaje(string mensaje)
+        {
+            const string caption = "Fin Del Juego";
+            var result = MessageBox.Show(mensaje, caption,
+                                         MessageBoxButtons.OK,
+                                         MessageBoxIcon.Error);
+        }
+
+        public bool continuar()
+        {
+            return presionado == juego.ElementAt(i);
+        }
+
+
     }
 }
